3917e7d1cc5d578515ff69200699d8fb45723086
Vinh <44563184+vinhltt@users.noreply.github.com>
2025-05-23 20:55:12 +0700

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1748008506675

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2bd162b4._.js",
+  "static/chunks/src_24b77cb8._.js"
+],
+    source: "dynamic"
+});
